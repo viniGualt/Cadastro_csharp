2525c27f10227f6826b8fa4c282daf9a4a49964c
Vinicius M <vini.gm.05@gmail.com>
2025-04-29 20:00:23 -0300

    Commit inicial

---
+++ ORM/Pedidos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM
+{
+    class Pedidos
+    {
+        Produtos produto { get; set; }
+        Clientes cliente { get; set; }
+        DateTime data { get; set; }
+        double valorTotal { get; set; }
+    }
+}
